85f93c2611a7da150e74f8434df13ced6affbb7f
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2020-02-05 15:14:36 +0100

    initial commit, clone from test

---
+++ includes/config.php
@@ -0,0 +1,40 @@
+<?php
+
+use Dotenv\Dotenv;
+use Dotenv\Repository\Adapter\EnvConstAdapter;
+use Dotenv\Repository\Adapter\ServerConstAdapter;
+use Dotenv\Repository\RepositoryBuilder;
+
+$adapters = [
+	new EnvConstAdapter(),
+	new ServerConstAdapter(),
+];
+
+$repository = RepositoryBuilder::create()
+    ->withReaders($adapters)
+    ->withWriters($adapters)
+    ->immutable()
+    ->make();
+
+Dotenv::create($repository, __DIR__ . '/../', null)->load();
+
+$configFile = json_decode(file_get_contents("https://test.lucacastelnuovo.nl/users/Luca-Castelnuovo/configuration/testingplatform.json"));
+
+$GLOBALS['config'] = (object) array(
+    'database' => (object) array(
+        'host' => $_ENV['DB_HOST'],
+        'port' => $_ENV['DB_PORT'],
+        'database' => $_ENV['DB_DATABASE'],
+        'user' => $_ENV['DB_USERNAME'],
+        'password' => $_ENV['DB_PASSWORD']
+    ),
+
+    'github' => (object) array(
+        'client_id' => $_ENV['GITHUB_CIENT_ID'],
+        'client_secret' => $_ENV['GITHUB_CLIENT_SECRET'],
+        'redirect' => 'https://test.lucacastelnuovo.nl'
+    ),
+
+    'allowed_users' => $configFile->allowed_users,
+    'allowed_extensions' => $configFile->allowed_extensions,
+);
---
+++ includes/init.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+require 'config.php';
+require 'auth.php';
+require 'files.php';
+require 'output.php';
+require 'projects.php';
+require 'security.php';
+require 'sql.php';
+require 'template.php';
+
+// External
+require '/var/www/cdn.lucacastelnuovo.nl/include.php';
